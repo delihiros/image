0dec9bb05d04e3f2296b5a11706bddc9d3702953
delihiros <delihiros@gmail.com>
2014-10-14 23:52:24 +0900

    fix dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :url "http://delihiros.github.io"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :resource-paths ["lib/imglib2-2.0.0-SNAPSHOT.jar"])
+  :dependencies [[org.clojure/clojure "1.6.0"]])
